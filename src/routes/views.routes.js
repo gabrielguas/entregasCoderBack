714fbfa0e72bee9f5607d597195b812344c34f43
Gabriel G <125676394+gabrielguas@users.noreply.github.com>
2023-12-05 01:08:00 -0300

    asda

---
+++ src/routes/views.routes.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import ProductManager from "../ProductManager.js";
+
+const router = Router();
+const pm = new ProductManager('./src/products.json');
+
+router.get('/', (req, res) => {
+    res.render("index", {
+        title: "Titulo",
+        nombre: "Gabriel"
+    });
+});
+
+router.get('/realtimeproducts', (req, res) => {
+    res.render("realTimeProducts.hbs", {
+        // seguir acá
+    });
+});
+
+router.get('/home', async (req, res) => {
+    try {
+        const productsData = await pm.getProducts(req, res);
+        res.render('home.hbs', { productsData });
+    } catch (error) {
+        console.error("Error al obtener productos:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+});
+
+export default router;
