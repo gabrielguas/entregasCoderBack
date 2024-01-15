8329c8afa9df1ef6bc13d0cdd88f8ae1dc84e294
Gabriel G <125676394+gabrielguas@users.noreply.github.com>
2024-01-14 22:14:57 -0300

    cambios

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,7 +1,36 @@
 import { Router } from "express";
 import productDao from "../Dao/DBManager/product.dao.js";
+import cookieParser from "cookie-parser";
 
 const router = Router();
+////////////////////////
+// Cookies
+
+router.use(cookieParser());
+
+// router.get("/setcookie", (req, res) => {
+//   //Sin firma
+//   //res.cookie("CooderCookie", "Esta es una cookie sin firma", { maxAge: 30000 }).send("Cookie asignada con exito");
+
+//   // Con firma
+//   res.cookie("CooderCookie", "Esta es una cookie con firma", { maxAge: 30000, signed: true }).send("Cookie asignada con exito");
+// });
+
+// router.get("/getcookie", (req, res) => {
+//   // Sin firma
+//   //res.send(req.cookies);
+
+//   // Con firma
+//   res.send(req.signedCookies);
+// });
+
+// router.get("/deletecookie",(req,res) =>{
+//   // Sin firma y con firma es lo mismo
+
+//   res.clearCookie("CooderCookie").send("CookieBorrada");
+// })
+
+//////////////////////
 
 router.get("/", (req, res) => {
   res.render("index", {
@@ -40,5 +69,55 @@ router.get("/products", async (req, res) => {
     res.status(500).json({ error: "Error al obtener productos" });
   }
 });
+
+// Sessions
+
+router.get("/session", (req, res) => {
+  if (req.session.counter) {
+    req.session.counter++;
+    res.send(`Se ha visitado este sitio ${req.session.counter} veces.`);
+  } else {
+    req.session.counter = 1;
+    res.send("Bienvenido!!");
+  }
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy((error) => {
+    if (error) {
+      res.json({
+        error: "Error logout",
+        msg: "Error al cerrar la sesion",
+      });
+    }
+    res.send("Session cerrada correctamente");
+  });
+});
+
+router.get("/login", (req, res) => {
+  const { username, password } = req.query;
+
+  if (username != "pepe" || password !== "123qwe") {
+    return res.status(401).send("Login failed, check your credentials");
+  } else {
+    req.session.user = username;
+    req.session.admin = false;
+    res.send("Login successful");
+  }
+});
+
+//Middleware auth
+
+function auth(req,res,next) {
+  if (req.session.user == 'pepe' && req.session.admin){
+    return next();
+  } else {
+    return res.status(403).send('Usuario no autorizado para ingresar a este recurso')
+  }
+}
+
+router.get('/private',auth, (req,res) => {
+  res.send('Estas autorizado');
+})
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ import { Server } from 'socket.io'
 import Handlebars from "handlebars";
 import { allowInsecurePrototypeAccess } from "@handlebars/allow-prototype-access";
 import  cartRouter  from './routes/carts.router.js'
+import session from "express-session";
+import FileStore from 'session-file-store'
+import MongoStore from "connect-mongo";
 
 const app = express();
 const httpServer = app.listen(8080, () => console.log("Server listening on port 8080"));
@@ -46,24 +49,35 @@ app.use(express.static(`${__dirname}/public`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", viewRouter)
+
+// Configuracion de Session
+
+const fileStore = FileStore(session)
+
+
+app.use(session(
+
+
+  {
+      //Usando -> session-file-store
+      //store: new fileStore({ path: "./sessions", ttl: 15, retries: 0}), 
+
+      // Usando connect-mongo
+      store: MongoStore.create({
+        mongoUrl:`mongodb+srv://guasgabriel22:FanoQ6mSsi7a1iwu@curso-backend-ch.j0ycecz.mongodb.net/entregaDB?retryWrites=true&w=majority`,
+        mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true},
+        ttl: 10 * 60
+      }),
+      secret: "coderS3cr3t",
+      resave: false, // guarda en memoria, no es necesario porque va a estar en un archivo
+      saveUninitialized: true // lo guarda al crear la session
+  }
+))
 
 // Routes
 app.use("/api/products", productRouter);
 app.use("/api/carts", cartRouter);
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use("/", viewRouter)
 
 
 
